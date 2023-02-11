fd4d4e02c33a73c5e8e79bf723838bb9bb70a8b8
Simen <simeeid@stud.ntnu.no>
2023-02-11 14:55:57 +0100

    Updated navBar component
    Updated component and stories navBar
    
    Issue: #17

--- src/components/atoms/Nav/NavBar.stories.tsx
+++ src/components/atoms/Nav/NavBar.stories.tsx
@@ -5,14 +5,12 @@ import { NavBar } from './NavBar';
 export default {
     title: 'atom/NavBar',
     component: NavBar,
-    args : { children : 'NavBar' },
+    args : {},
 };
 
-export const Default = {}
+export const Default = { args : {title : 'VAMOS', backgroundtype : 'blur', children : 'NavBar'} }
 
-export const Title = { args : { title : 'Title' } }
-export const TitleAndChildren = { args : { title : 'Title', children: "Children" } }
-export const TitleAndChildrenAndProps = { args : { title : 'Title', children: "Children", color: "red" } }
-export const TitleAndProps = { args : { title : 'Title', color: "red" } }
-export const ChildrenAndProps = { args : { children: "Children", color: "red" } }
-export const Props = { args : { color: "red" } }
+export const Clear = { args : {title : 'Title', backgroundtype : 'clear', children : 'Clear'} }
+export const Blur = { args : {title : 'Title', backgroundtype : 'blur', children : 'Blur'} }
+
+
--- src/components/atoms/Nav/NavBar.tsx
+++ src/components/atoms/Nav/NavBar.tsx
@@ -1,23 +1,45 @@
 //create component navbar
 
 import { Box } from '@chakra-ui/react'
+import { FaKey, FaUserAlt, FaUserCircle } from "react-icons/fa";
+import { SiHackthebox } from "react-icons/si";
 interface BoxProps {
     title: string;
+    backgroundtype?: "clear" | "blur";
     [key: string]: any;
 }
-export const NavBar = ({ title, ...props }: BoxProps) => {
+export const NavBar: React.FC<BoxProps> = ({ title, backgroundtype, ...props }: BoxProps) => {
+
+    let backdropfilter = undefined;
+    let bordercolor = undefined;
+    let borderwidth = undefined;
+    let fontcolor = undefined;
+    let fontcolor2 = undefined;
+    let backgroundimage = undefined;
+    if (backgroundtype) {
+        if (backgroundtype === "clear") backdropfilter="none", bordercolor = "none", borderwidth="0", fontcolor="white", fontcolor2="black", backgroundimage="linear-gradient(white, white)";
+        if (backgroundtype === "blur") backdropfilter="blur(5px)", bordercolor = "white", borderwidth="1px", fontcolor="black", fontcolor2="white", backgroundimage="linear-gradient(to right, darkblue, black)";
+    }
+    
+    let style0 = {position:"relative", left:"0", width:"31%", marginTop:"0px", height:"100%"};
+    let style1 = {position:"relative", left:"0", width:"23%", marginTop:"0px", height:"100%"};
+    let style2 = {bottom:'0', alignItems: "center", display: "flex", justifyContent: "center"};
+
     return (
-        <Box borderWidth='1px' borderRadius='lg' display='flex' alignItems='baseline' {...props}>
-            {title}
-            <Box as='button' borderRadius='md' bg='tomato' color='white' px={4} h={8}>
-                DASHBOARD</Box>
-            <Box as='button' borderRadius='md' bg='tomato' color='white' px={4} h={8}>
-                PROFILE</Box>
-            <Box as='button' borderRadius='md' bg='tomato' color='white' px={4} h={8}>
-                SIGN UP</Box>
-            <Box as='button' borderRadius='md' bg='tomato' color='white' px={4} h={8}>
-                SIGN IN</Box>
-            <Box as='button' borderRadius='md' bg='tomato' color='white' px={4} h={8}>
+        <Box backdropFilter={backdropfilter} borderColor={bordercolor} borderWidth={borderwidth} borderRadius='12px' display='flex' alignItems='baseline' h='50px' {...props}>
+            <Box as='button' style={{position:"sticky", marginLeft:"10px", width:"120px", height:"30px", marginTop:"10px"}} borderRadius='md' color={fontcolor} >
+                {title}</Box>
+            <Box style={{position:"relative", marginLeft:"auto", marginRight:"auto", width:"500px", marginTop:"10px", height:"30px"}}>
+                <Box as='button' style={style0} borderRadius='md' color={fontcolor} px={4} h={8}>
+                    <Box style={style2}><SiHackthebox/>&nbsp;DASHBOARD</Box></Box>
+                <Box as='button' style={style1} borderRadius='md' color={fontcolor} px={4} h={8}>
+                    <Box style={style2}><FaUserAlt/>&nbsp;PROFILE</Box></Box>
+                <Box as='button' style={style1} borderRadius='md' color={fontcolor} px={4} h={8}>
+                    <Box style={style2}><FaUserCircle/>&nbsp;SIGN UP</Box></Box>
+                <Box as='button' style={style1} borderRadius='md' color={fontcolor} px={4} h={8}>
+                    <Box style={style2}><FaKey/>&nbsp;SIGN IN</Box></Box>
+            </Box>
+            <Box as='button' style={{position:'sticky', marginRight:"10px", width:"130px", marginTop:"10px", height:"30px"}} borderRadius='20px' color={fontcolor2} backgroundImage={backgroundimage} px={4} h={8}>
                 ALT BUTTON</Box>
             </Box>
     )
