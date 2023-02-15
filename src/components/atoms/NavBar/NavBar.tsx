37ef1beb93165a249ed5e338f4db40b2242c010e
Simen <simeeid@stud.ntnu.no>
2023-02-15 18:55:38 +0100

    Refactored styling
    Refactored the styling attributes of NavBar
    
    Issue: #17

--- src/components/atoms/NavBar/NavBar.tsx
+++ src/components/atoms/NavBar/NavBar.tsx
@@ -18,11 +18,30 @@ export const NavBar: React.FC<BoxProps> = ({backgroundtype, ...props }: BoxProps
     let backgroundcolor = undefined;
     let shadowvalue = undefined;
     if (backgroundtype) {
-        if (backgroundtype === "clear") backdropfilter="none", bordercolor = "none", borderwidth="0", fontcolor="white", fontcolor2="black", backgroundimage="linear-gradient(white, white)", backgroundcolor="none", shadowvalue="none";
-        if (backgroundtype === "blur") backdropfilter="blur(5px)", bordercolor = "white", borderwidth="1px", fontcolor="black", fontcolor2="white", backgroundimage="linear-gradient(to right, darkblue, black)", backgroundcolor="rgba(255, 255, 255, 0.5)", shadowvalue="2xl";
+        if (backgroundtype === "clear") {
+            backdropfilter="none"
+            bordercolor = "none"
+            borderwidth="0"
+            fontcolor="white"
+            fontcolor2="black"
+            backgroundimage="linear-gradient(white, white)"
+            backgroundcolor="none"
+            shadowvalue="none"
+        }
+        if (backgroundtype === "blur") {
+            backdropfilter="blur(5px)"
+            bordercolor = "white"
+            borderwidth="1px"
+            fontcolor="black"
+            fontcolor2="white"
+            backgroundimage="linear-gradient(to right, darkblue, black)"
+            backgroundcolor="rgba(255, 255, 255, 0.5)"
+            shadowvalue="2xl"
+        }
     }
     
-    let style0 = {position:"relative", left:"0", width:"31%", marginTop:"0px", height:"100%", fontSize:"14px"};
+    let style0 = {
+        position:"relative", left:"0", width:"31%", marginTop:"0px", height:"100%", fontSize:"14px"};
     let style1 = {position:"relative", left:"0", width:"23%", marginTop:"0px", height:"100%", fontSize:"14px"};
     let style2 = {bottom:'0', alignItems: "center", display: "flex", justifyContent: "center"};
 
@@ -31,13 +50,13 @@ export const NavBar: React.FC<BoxProps> = ({backgroundtype, ...props }: BoxProps
             <Box as='button' style={{position:"sticky", marginLeft:"10px", width:"120px", height:"30px", marginTop:"10px"}} borderRadius='md' color={fontcolor} >
                 VAMOS</Box>
             <Box style={{position:"relative", marginLeft:"auto", marginRight:"auto", width:"450px", marginTop:"10px", height:"30px"}}>
-                <Box as='button' style={style0} borderRadius='md' color={fontcolor} px={2} h={8}>
+                <Box as='button' style={Object.assign(style0)} borderRadius='md' color={fontcolor} px={2} h={8}>
                     <Box style={style2}><SiHackthebox/>&nbsp;DASHBOARD</Box></Box>
-                <Box as='button' style={style1} borderRadius='md' color={fontcolor} px={2} h={8}>
+                <Box as='button' style={Object.assign(style1)} borderRadius='md' color={fontcolor} px={2} h={8}>
                     <Box style={style2}><FaUserAlt/>&nbsp;PROFILE</Box></Box>
-                <Box as='button' style={style1} borderRadius='md' color={fontcolor} px={2} h={8}>
+                <Box as='button' style={Object.assign(style1)} borderRadius='md' color={fontcolor} px={2} h={8}>
                     <Box style={style2}><FaUserCircle/>&nbsp;SIGN UP</Box></Box>
-                <Box as='button' style={style1} borderRadius='md' color={fontcolor} px={2} h={8}>
+                <Box as='button' style={Object.assign(style1)} borderRadius='md' color={fontcolor} px={2} h={8}>
                     <Box style={style2}><FaKey/>&nbsp;SIGN IN</Box></Box>
             </Box>
             <Box as='button' style={{position:'sticky', marginRight:"10px", width:"130px", marginTop:"10px", height:"30px"}} borderRadius='20px' color={fontcolor2} backgroundImage={backgroundimage} px={4} h={8}>
