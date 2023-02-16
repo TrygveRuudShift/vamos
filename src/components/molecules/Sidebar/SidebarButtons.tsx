8a969faebcba3f2fa542743adb0138669ea87bb0
Jakob Relling <jakobere@stud.ntnu.no>
2023-02-16 09:22:26 +0100

    fix: sidepanelbutton molecule

--- src/components/molecules/Sidebar/SidebarButtons.tsx
+++ src/components/molecules/Sidebar/SidebarButtons.tsx
@@ -1,5 +1,7 @@
 import { SidepanelButton } from "../../atoms/SidepanelButton/SidepanelButton";
 import { Flex } from "@chakra-ui/react";
+import { AiFillHome, AiFillStar } from "react-icons/ai";
+import { MdBackpack } from "react-icons/md";
 
 interface ButtonsProps {
   size?: "small" | "medium" | "large";
@@ -13,7 +15,7 @@ interface ButtonsProps {
 export const SidebarButtons: React.FC<ButtonsProps> = ({onClick, children, ...props}) => {
   
   onClick = () => {
-    
+
   }
   return (
     <Flex
@@ -21,9 +23,9 @@ export const SidebarButtons: React.FC<ButtonsProps> = ({onClick, children, ...pr
       gap={props.gapSize}
       h={props.height}
       w={props.width}>
-      <SidepanelButton selected={false} href={""} iconString={"AiFillHome"} size={props.size}>Home</SidepanelButton>
-      <SidepanelButton selected={true} href={""} iconString={"MdBackPack"} size={props.size}>Backpack</SidepanelButton>
-      <SidepanelButton selected={false} href={""} iconString={"AiFillStar"} size={props.size}>Rated</SidepanelButton>
+      <SidepanelButton selected={false} href={""} icon={AiFillHome} size={props.size}>Home</SidepanelButton>
+      <SidepanelButton selected={true} href={""} icon={AiFillStar} size={props.size}>Rated</SidepanelButton>
+      <SidepanelButton selected={false} href={""} icon={MdBackpack} size={props.size}>Backpack</SidepanelButton>
     </Flex>
   );
     
